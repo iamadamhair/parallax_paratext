914ef17d21d59a26425ffda52566a96d4ba45628
Michael Bass <littlebass09@neo.tamu.edu>
2016-11-17 00:53:02 -0600

    MB Animation working better, text color change animated, footnotes and glosses added

--- js/Index.js
+++ js/Index.js
@@ -1,9 +1,9 @@
-
+var currentBackground = 1;
 var darkenStanzaPercentage = "40%";
 
 var darkenPercentage = "70%";
 var lightenPercentage = "40%";
-var backgroundSlideTime = 1.5;
+//var backgroundSlideTime = 1.5;
 
 function DarkenText(id) {
     var item = document.getElementById(id);
@@ -29,30 +29,98 @@ function LightenText(id) {
 //        DarkenText(this.element.id);
 //}, { offset: lightenPercentage })
 
+$('.containsNote').mouseover(function () {
+    //var note = document.getElementById(this.id.concat("Note"));
+    $("#".concat(this.id).concat("Note")).removeClass("exit");
+    $("#".concat(this.id).concat("Note")).addClass("enter");
+
+})
+
+$('.containsNote').mouseout(function () {
+    //document.getElementById(this.id).style.color = "blue";
+    $("#".concat(this.id).concat("Note")).removeClass("enter");
+    $("#".concat(this.id).concat("Note")).addClass("exit");
+})
+
 $('.stanza').waypoint(function (direction) {
     var stanzaNumber = parseInt((this.element.id).match(/\d+$/)[0], 10);
-    DarkenStanza(stanzaNumber);
     if (direction == "down")
+    {
+        DarkenStanzaDown(stanzaNumber);
         AnimateDown(stanzaNumber);
+    }
     else
+    {
+        DarkenStanzaUp(stanzaNumber);
         AnimateUp(stanzaNumber);
+    }
 //      DarkenText(this.element.id);
 }, { offset: darkenStanzaPercentage })
 
+function MakeBlack(stanzaNumber) {
+    $("#stanza".concat((stanzaNumber).toString())).removeClass("lightGray");
+    $("#stanza".concat((stanzaNumber).toString())).addClass("black");
+}
+
+function MakeLightGray(stanzaNumber) {
+    $("#stanza".concat((stanzaNumber).toString())).removeClass("black");
+    $("#stanza".concat((stanzaNumber).toString())).addClass("lightGray");
+}
 
 function DarkenStanza(stanzaNumber) {
-    if(stanzaNumber > 0)
-    {
-        document.getElementById("stanza".concat((stanzaNumber - 1).toString())).style.color = "lightgray";
+    if (stanzaNumber % 3 == 1) {
+        if (stanzaNumber > 3) {
+            //document.getElementById("stanza".concat((stanzaNumber - 1).toString())).style.color = "lightgray";
+            //document.getElementById("stanza".concat((stanzaNumber - 2).toString())).style.color = "lightgray";
+            //document.getElementById("stanza".concat((stanzaNumber - 3).toString())).style.color = "lightgray";
+            MakeLightGray(stanzaNumber - 1);
+            MakeLightGray(stanzaNumber - 2);
+            MakeLightGray(stanzaNumber - 3);
+        }
+        //document.getElementById("stanza".concat((stanzaNumber).toString())).style.color = "black";
+        //document.getElementById("stanza".concat((stanzaNumber + 1).toString())).style.color = "black";
+        //document.getElementById("stanza".concat((stanzaNumber + 2).toString())).style.color = "black";
+        MakeBlack(stanzaNumber);
+        MakeBlack(stanzaNumber + 1);
+        MakeBlack(stanzaNumber + 2);
+        //document.getElementById("stanza".concat((stanzaNumber + 3).toString())).style.color = "lightgray";
+        //document.getElementById("stanza".concat((stanzaNumber + 4).toString())).style.color = "lightgray";
+        //document.getElementById("stanza".concat((stanzaNumber + 5).toString())).style.color = "lightgray";
+        MakeLightGray(stanzaNumber + 3);
+        MakeLightGray(stanzaNumber + 4);
+        MakeLightGray(stanzaNumber + 5);
     }
-    document.getElementById("stanza".concat((stanzaNumber + 1).toString())).style.color = "lightgray";
-    document.getElementById("stanza".concat((stanzaNumber).toString())).style.color = "black";
+}
+
+function DarkenStanzaDown(stanzaNumber) {
+    if (stanzaNumber % 3 == 1) {
+        if (stanzaNumber > 3) {
+            MakeLightGray(stanzaNumber - 1);
+            MakeLightGray(stanzaNumber - 2);
+            MakeLightGray(stanzaNumber - 3);
+        }
+        MakeBlack(stanzaNumber);
+        MakeBlack(stanzaNumber + 1);
+        MakeBlack(stanzaNumber + 2);
+    }
+}
+
+function DarkenStanzaUp(stanzaNumber) {
+    if (stanzaNumber % 3 == 1) {
+        MakeBlack(stanzaNumber);
+        MakeBlack(stanzaNumber + 1);
+        MakeBlack(stanzaNumber + 2);
+        MakeLightGray(stanzaNumber + 3);
+        MakeLightGray(stanzaNumber + 4);
+        MakeLightGray(stanzaNumber + 5);
+    }
 }
 
 function AnimateUp(number) {
-    if (number > 1) {
-        $("#background".concat((number - 1).toString())).removeClass("exit");
-        $("#background".concat((number - 1).toString())).addClass("enter");
+    var select = Math.floor(number / 3) + number % 3;
+    if (number%3 == 1 & select != currentBackground) {
+        $("#background".concat((select).toString())).removeClass("exit");
+        $("#background".concat((select).toString())).addClass("enter");
     }
     //if (number == 1)
     //    Animate1Up();
@@ -62,7 +130,12 @@ function AnimateUp(number) {
 }
 
 function AnimateDown(number) {
-    $("#background".concat((number - 1).toString())).addClass("exit");
+    var select = Math.floor(number/3) + number%3 - 1;
+    if (number % 3 == 1)
+    {
+        $("#background".concat((select).toString())).addClass("exit");
+        currentBackground = select + 1;
+    }
     //if (number == 1)
     //    Animate1Down();
     //if (number == 2)
